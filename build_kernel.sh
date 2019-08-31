c8701a20adae31fb86ec0ee18fa880ba439c3837
Wolfram Liebchen <achalmgucker@googlemail.com>
2019-08-31 23:16:48 +0200

    Start with Linux 3.18.91 from Samsung SM-J730F (Galaxy J7 (2017)

---
+++ build_kernel.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+export ARCH=arm64
+export ANDROID_MAJOR_VERSION=p
+export CROSS_COMPILE=${ANDROID_TOOLCHAIN}/aarch64-linux-android-
+
+#make  exynos7870-j7y17lte_defconfig
+#make -j64
+
